4e693f61a6ca7bd7b9ad117c79179c97e55359db
Paul Strydom <pauls.mts@gmail.com>
2022-04-20 18:22:44 +0200

    Improvement
    Improved the function to check that numbers at the same list index is not added up and compared to num

--- Day1 Coding Challenge.py
+++ Day1 Coding Challenge.py
@@ -1,9 +1,16 @@
-def sum_of_list(lst, num):
-    for n in lst:
-        for x in lst:
-            if n != x and n + x == num:
-                return True
-    return False
+def list_addup(check_list: list, num: int) -> bool:
+  """
+  Takes a list and a int as paramenters 
+  and checks if 2 of its unique members sums up to the
+  supplied number parameter and return a boolean if, the 
+  sum is possible
+  """
+  for i in check_list:
+    for j in check_list:
+      if i + j == num and check_list.index(i) != check_list.index(j):
+        return True
+
+  return False
     
 print(sum_of_list([10, 15, 3, 7], 17))
             
